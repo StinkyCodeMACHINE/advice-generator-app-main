dcb36390a9e79bbfd0f2b266ab473371fb5853a7
PoopFace <danilsteam-88@hotmail.com>
2023-11-05 23:11:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [https://luminous-figolla-ea91da.netlify.app/](https://luminous-figolla-ea91da.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Preprocessor SCSS
+- JavaScript
+- React
+
+## Author
+
+- Frontend Mentor - [@StinkyCodeMACHINE](https://www.frontendmentor.io/profile/StinkyCodeMACHINE)
---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [advice, setAdvice] = useState({});
+
+  useState(() => {
+    async function getAdvice() {
+      const { data } = await axios.get(`https://api.adviceslip.com/advice`);
+      setAdvice(data.slip);
+    }
+
+    getAdvice();
+  }, []);
+
+  return (
+    <>
+      <main className="advice-box">
+        <h1 className="sr-only">Advice generator app</h1>
+        <h2 className="advice-box__id">ADVICE #{advice.id}</h2>
+        <p className="advice-box__text">“{advice.advice}”</p>
+        <img className="advice-box__divider" alt="" />
+        <div
+          onClick={async () => {
+            const { data } = await axios.get(
+              `https://api.adviceslip.com/advice?cb=${Date.now()}`
+            );
+            setAdvice(data.slip);
+          }}
+          className="advice-box__dice-container"
+        >
+          <img
+            className="advice-box__dice"
+            src="./assets/icon-dice.svg"
+            alt="dice"
+          />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
